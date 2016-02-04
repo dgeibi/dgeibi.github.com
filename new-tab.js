7e79ad7073e68f617fdaaf041065563ef5214157
dgeibi <pnginside@gmail.com>
2016-02-04 16:53:57 +0800

    external links in new tab now

---
+++ new-tab.js
@@ -0,0 +1,9 @@
+// from http://stackoverflow.com/a/4425214
+
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+       links[i].target = '_blank';
+   }
+}
