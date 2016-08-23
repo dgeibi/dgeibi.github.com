4ff2095131cefa73e5015466bf304f9d2172a7a2
dgeibi <png.inside@gmail.com>
2016-08-23 21:23:12 +0800

    update at 2016-08-23 13:23:12 UTC

--- js/content.js
+++ js/content.js
@@ -1,109 +1,110 @@
-var children = function children(childNodes, reg) {
-        var result = [],
-            isReg = typeof reg === 'object',
-            isStr = typeof reg === 'string',
-            node, i, len;
+// copy from http://blog.lxjwlt.com/front-end/2014/07/06/js-create-directory.html
+function children(childNodes, reg) {
+    var result = [],
+        isReg = typeof reg === 'object',
+        isStr = typeof reg === 'string',
+        node, i, len;
 
-        for (i = 0, len = childNodes.length; i < len; i++) {
-            node = childNodes[i];
+    for (i = 0, len = childNodes.length; i < len; i++) {
+        node = childNodes[i];
 
-            if ((node.nodeType === 1 || node.nodeType === 9) &&
-                (!reg ||
-                    isReg && reg.test(node.tagName.toLowerCase()) ||
-                    isStr && node.tagName.toLowerCase() === reg)) {
+        if ((node.nodeType === 1 || node.nodeType === 9) &&
+            (!reg ||
+                isReg && reg.test(node.tagName.toLowerCase()) ||
+                isStr && node.tagName.toLowerCase() === reg)) {
 
-                result.push(node);
-            }
+            result.push(node);
         }
-        return result;
-    },
+    }
+    return result;
+}
 
-    // 创建目录
-    createDirectory = function(article, directory, isDirNum) {
-        var contentArray = [],
-            titleId = [],
-            levelArray, root, level,
-            currentList, list, li, link, i, len;
+// 创建目录
+function createDirectory(article, directory, isDirNum) {
+    var contentArray = [],
+        titleId = [],
+        levelArray, root, level,
+        currentList, list, li, link, i, len;
 
-        // 获取标题编号 标题内容
-        levelArray = (function(article, contentArray, titleId) {
-            var titleElem = children(article.childNodes, /^h\d$/),
-                levelArray = [],
-                lastNum = +titleElem[0].tagName.match(/\d/)[0],
-                lastRevNum = 1,
-                count = 0,
-                lastRevNum, num, elem, rootNum = lastNum;
+    // 获取标题编号 标题内容
+    levelArray = (function(article, contentArray, titleId) {
+        var titleElem = children(article.childNodes, /^h\d$/),
+            levelArray = [],
+            lastNum = +titleElem[0].tagName.match(/\d/)[0],
+            lastRevNum = 1,
+            count = 0,
+            lastRevNum, num, elem, rootNum = lastNum;
 
-            while (titleElem.length) {
-                elem = titleElem.shift();
+        while (titleElem.length) {
+            elem = titleElem.shift();
 
-                // 保存标题内容
-                contentArray.push(elem.innerHTML);
+            // 保存标题内容
+            contentArray.push(elem.innerHTML);
 
-                // 修正
-                num = +elem.tagName.match(/\d/)[0] - rootNum + 1;
+            // 修正
+            num = +elem.tagName.match(/\d/)[0] - rootNum + 1;
 
-                if (num > lastNum) {
-                    levelArray.push(1);
-                    lastRevNum += 1;
-                } else if (num === lastRevNum ||
-                    (num > lastRevNum && num <= lastNum)) {
-                    levelArray.push(0);
-                    lastRevNum = lastRevNum;
-                } else if (num < lastRevNum) {
-                    levelArray.push(num - lastRevNum);
-                    lastRevNum = num;
-                }
-
-                count += levelArray[levelArray.length - 1];
-                lastNum = num;
-
-                // 添加标识符
-                elem.id = elem.id || elem.innerHTML.toLowerCase().replace(/[\s*+?^=!:${}()|[\]\/\\]/g, "").replace(/<.*>(.*)<.*>/g, '$1')
-                titleId.push(elem.id);
+            if (num > lastNum) {
+                levelArray.push(1);
+                lastRevNum += 1;
+            } else if (num === lastRevNum ||
+                num > lastRevNum && num <= lastNum) {
+                levelArray.push(0);
+                lastRevNum = lastRevNum;
+            } else if (num < lastRevNum) {
+                levelArray.push(num - lastRevNum);
+                lastRevNum = num;
             }
 
-            // 避免一开始就进入下一层
-            if (count !== 0 && levelArray[0] === 1) levelArray[0] = 0;
+            count += levelArray[levelArray.length - 1];
+            lastNum = num;
 
-            return levelArray;
-        })(article, contentArray, titleId);
+            // 添加标识符
+            elem.id = elem.id || elem.innerHTML.toLowerCase().replace(/[\s*+?^=!:${}()|[\]\/\\]/g, "").replace(/<.*>(.*)<.*>/g, '$1')
+            titleId.push(elem.id);
+        }
 
-        // 构造目录
-        currentList = root = document.createElement('ul');
-        var dirNum = [0];
-        for (i = 0, len = levelArray.length; i < len; i++) {
-            level = levelArray[i];
-            if (level === 1) {
-                list = document.createElement('ul');
-                if (!currentList.lastElementChild) {
-                    currentList.appendChild(document.createElement('li'));
-                }
-                currentList.lastElementChild.appendChild(list);
-                currentList = list;
-                dirNum.push(0);
-            } else if (level < 0) {
-                level *= 2;
-                while (level++) {
-                    if (level % 2) dirNum.pop();
-                    currentList = currentList.parentNode;
-                }
+        // 避免一开始就进入下一层
+        if (count !== 0 && levelArray[0] === 1) levelArray[0] = 0;
+
+        return levelArray;
+    })(article, contentArray, titleId);
+
+    // 构造目录
+    currentList = root = document.createElement('ul');
+    var dirNum = [0];
+    for (i = 0, len = levelArray.length; i < len; i++) {
+        level = levelArray[i];
+        if (level === 1) {
+            list = document.createElement('ul');
+            if (!currentList.lastElementChild) {
+                currentList.appendChild(document.createElement('li'));
             }
-            dirNum[dirNum.length - 1]++;
-            li = document.createElement('li');
-            link = document.createElement('a');
-            link.href = '#' + titleId[i];
-            link.innerHTML = !isDirNum ? contentArray[i] :
-                dirNum.join('.') + '. ' + contentArray[i];
-            li.appendChild(link);
-            currentList.appendChild(li);
+            currentList.lastElementChild.appendChild(list);
+            currentList = list;
+            dirNum.push(0);
+        } else if (level < 0) {
+            level *= 2;
+            while (level++) {
+                if (level % 2) dirNum.pop();
+                currentList = currentList.parentNode;
+            }
         }
+        dirNum[dirNum.length - 1]++;
+        li = document.createElement('li');
+        link = document.createElement('a');
+        link.href = '#' + titleId[i];
+        link.innerHTML = !isDirNum ? contentArray[i] :
+            dirNum.join('.') + '. ' + contentArray[i];
+        li.appendChild(link);
+        currentList.appendChild(li);
+    }
 
-        if (len) {
-            directory.innerHTML = "<h2>目录</h2>";
-            directory.appendChild(root);
-        }
-    };
+    if (len) {
+        directory.innerHTML = "<h2>目录</h2>";
+        directory.appendChild(root);
+    }
+}
 
 try {
     createDirectory(document.getElementsByClassName('post-content')[0],
