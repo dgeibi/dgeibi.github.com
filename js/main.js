8df6224a42aaab4c590201dbe5393ad5ddfa15c0
dgeibi <png.inside@gmail.com>
2016-08-24 10:59:10 +0800

    update at 2016-08-24 02:59:10 UTC

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 // from http://stackoverflow.com/a/4425214
 var links = document.links;
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-   if (links[i].hostname != window.location.hostname) {
-       links[i].target = '_blank';
-   }
+    if (links[i].hostname != window.location.hostname) {
+        links[i].target = '_blank';
+    }
 }
 
 // add table-wrapper
 var tables = document.getElementsByTagName("table");
 for (var i = 0, len = tables.length; i < len; i++) {
-	var div = document.createElement("div");
-	div.className = "table-wrapper";
-	var range = document.createRange();
-	range.selectNode(tables[i]);
-	range.surroundContents(div);
+    var div = document.createElement("div");
+    div.className = "table-wrapper";
+    var range = document.createRange();
+    range.selectNode(tables[i]);
+    range.surroundContents(div);
 }
