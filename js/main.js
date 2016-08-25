69f7ec56f35c0853fdcfe10ba3da17141949e150
dgeibi <png.inside@gmail.com>
2016-08-25 14:13:41 +0800

    update at 2016-08-25 06:13:41 UTC

--- js/main.js
+++ js/main.js
@@ -29,10 +29,25 @@ $(document).ready(function() {
         }
     });
 
-    $("#toTop").on('click', function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function() {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function() {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function(e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 500);
+        });
+    }
 })
